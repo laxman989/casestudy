6ca0844d0e45439371ef7120490e61e29b0d2247
Laxman Choudhary <55774606+laxman989@users.noreply.github.com>
2022-03-11 08:34:31 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,21 +5,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
+    <title>Laxman</title>
 </head>
 <body>
-    <div class="hero">
-        <div class="navbar">
-            <img src="images/logo.png" class="logo">
-            <button type="button">Sign up</button>
-        </div>
         <div class="content">
             <h3>Welcome to our</h3>
             <h1>Worlds <br>most loved store</h1>
             <button type="button">Get Started</button>
         </div>
-        </div>
-    </div>
 </body>
 </html>'
 ?>
