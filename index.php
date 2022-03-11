24935689d76ec19d9a48d3c0523bdde2ca4b44bb
Laxman Choudhary <55774606+laxman989@users.noreply.github.com>
2022-03-11 08:31:16 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+    echo '
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+<body>
+    <div class="hero">
+        <div class="navbar">
+            <img src="images/logo.png" class="logo">
+            <button type="button">Sign up</button>
+        </div>
+        <div class="content">
+            <h3>Welcome to our</h3>
+            <h1>Worlds <br>most loved store</h1>
+            <button type="button">Get Started</button>
+        </div>
+        <div class="sidebar">
+            <img src="images/menu.png" class="menu">
+
+            <div class="social-link">
+                <img src="images/fb.png">
+                <img src="images/ig.png">
+                <img src="images/tw.png">
+            </div>
+
+            <div class="useful-link">
+                <img src="images/share.png">
+                <img src="images/info.png">
+            </div>
+        </div>
+        <div class="bubbles">
+            <img src="images/bubble.png">
+            <img src="images/bubble.png">
+            <img src="images/bubble.png">
+            <img src="images/bubble.png">
+            <img src="images/bubble.png">
+            <img src="images/bubble.png">
+            <img src="images/bubble.png">
+            <img src="images/bubble.png">
+        </div>
+    </div>
+</body>
+</html>'
+?>
